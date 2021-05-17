9ed93565336fb9bbb5a4b5da9834b4980847fd2c
arabnejad <hamid.arabnejad@gmail.com>
2021-05-17 17:27:41 +0100

    Adds GitHub actions for pytest

--- tests/test_fabflee.py
+++ tests/test_fabflee.py
@@ -15,10 +15,51 @@ logger = logging.getLogger(__name__)
 # GitHub action = 2 cores
 
 
+def test_mali(run_py):
+    ret = run_py("mali", "10")
+    assert ret == "OK"
+
+
 def test_par_mali(run_par):
     logger.addHandler(logging.StreamHandler(sys.stdout))
     logger.setLevel(logging.DEBUG)
     ret = run_par("mali", "10", "2")
+    assert ret == "OK"
+
+
+def test_burundi(run_py):
+    ret = run_py("burundi", "10")
+    assert ret == "OK"
+
+
+def test_par_burundi(run_par):
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.DEBUG)
+    ret = run_par("burundi", "10", "2")
+    assert ret == "OK"
+
+
+def test_car(run_py):
+    ret = run_py("car", "10")
+    assert ret == "OK"
+
+
+def test_par_car(run_par):
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.DEBUG)
+    ret = run_par("car", "10", "2")
+    assert ret == "OK"
+
+
+def test_ssudan(run_py):
+    ret = run_py("ssudan", "10")
+    assert ret == "OK"
+
+
+def test_par_ssudan(run_par):
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.DEBUG)
+    ret = run_par("ssudan", "10", "2")
     assert ret == "OK"
 
 
